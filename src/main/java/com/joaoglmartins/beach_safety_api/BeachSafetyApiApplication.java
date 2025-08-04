71acffcf7d0626a56beb6a99c03245de6b414db7
User <User@DESKTOP-J7S35OR.Home>
2025-08-04 19:06:50 +0100

    "Initial commit of beach safety API"

---
+++ src/main/java/com/joaoglmartins/beach_safety_api/BeachSafetyApiApplication.java
@@ -0,0 +1,13 @@
+package com.joaoglmartins.beach_safety_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeachSafetyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeachSafetyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaomartins/beach_safety_api/BeachSafetyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaomartins.beach_safety_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeachSafetyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
